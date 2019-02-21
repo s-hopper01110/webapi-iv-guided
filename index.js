366bd20a506576b3e0baadde551df34fe2090a10
Sheila <s.hopper01110@gmail.com>
2019-02-21 12:56:00 -0500

    testing deployment

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http:localhost ${port} ***\n`);
 });
+
+
+//dev > qa >functional testing > qa > production
