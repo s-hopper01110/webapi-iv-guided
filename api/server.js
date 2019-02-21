4104518df28e7d78a4c6a66f77bc718788a839df
Sheila <s.hopper01110@gmail.com>
2019-02-21 13:22:46 -0500

    Adding test messages

--- api/server.js
+++ api/server.js
@@ -8,10 +8,21 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+// server.get('/', async (req, res) => {
+//   try {
+//     const shoutouts = await db('shoutouts');
+//     res.status(200).json(shoutouts);
+//   } catch (error) {
+//     console.error('\nERROR', error);
+//     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+//   }
+// });
+
 server.get('/', async (req, res) => {
   try {
     const shoutouts = await db('shoutouts');
-    res.status(200).json(shoutouts);
+    const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
   } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
